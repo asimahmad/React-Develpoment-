76ea05b4336ce9622c5471c1b62771c5719e8157
Asim <asim005011997@gmail.com>
2021-04-13 14:13:27 +0530

    When have to update the state based on previous state then pass a function as an argumnet instead of the reqular object

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -10,9 +10,15 @@ class Counter extends Component {
     
     increment(){
         //this.state.count = this.state.count +1; won't work in rendering, never modify the state directly use setState
-        this.setState({
-            count: this.state.count +1
-        }, () =>{ console.log('Callback value:' + this.state.count)})
+        // this.setState({
+        //     count: this.state.count +1
+        // }, () =>{ console.log('Callback value:' + this.state.count)})
+        // console.log(this.state.count)
+
+
+        this.setState(prevState => ({
+            count: prevState.count +1
+        }))
         console.log(this.state.count)
     }
 
